20dd5df2b0dd2349634d7e36fbe44d4b7a6d5832
Michael2150 <52236517+Michael2150@users.noreply.github.com>
2022-04-11 12:47:23 +0100

    Dijkstra's Algorithm Done

--- src/main/java/com/ca/two/graph/Graph.java
+++ src/main/java/com/ca/two/graph/Graph.java
@@ -23,16 +23,45 @@ public class Graph<T> {
         return true;
     }
 
+    //Adds a node to the Graph. and connects it to the given nodes.
+    public boolean addNode(T newData, T... neighbors) {
+        if (contains(newData))
+            return false;
+
+        nodes.add(new Node<T>(newData));
+        for (T n : neighbors) {
+            addEdge(newData, n);
+        }
+        return true;
+    }
+
+    //Get weight between two nodes.
+    public float getWeight(T node1, T node2) {
+        if (!contains(node1) || !contains(node2))
+            throw new IllegalArgumentException("One of the nodes does not exist.");
+
+        Node<T> n1 = getNode(node1);
+        assert n1 != null;
+
+        Node<T> n2 = getNode(node2);
+        assert n2 != null;
+
+        return n1.getEdgeWeight(n2);
+    }
+
     //Add an Edge to the Graph.
     //Returns true if the edge was added, false if it was not.
     public boolean addEdge(T from, T to) {
+        return addEdge(from, to, 0.5f);
+    }
+    public boolean addEdge(T from, T to, float weight) {
         if (!contains(from) || !contains(to))
             throw new IllegalArgumentException("One of the nodes does not exist.");
 
         Node<T> fromNode = getNode(from);
         Node<T> toNode = getNode(to);
 
-        fromNode.addEdge(toNode);
+        fromNode.addEdge(toNode, weight);
         return true;
     }
 
@@ -57,10 +86,34 @@ public class Graph<T> {
             throw new IllegalArgumentException("One of the nodes does not exist.");
 
         Node<T> fromNode = getNode(from);
+        assert fromNode != null;
+
         Node<T> toNode = getNode(to);
 
         fromNode.removeEdge(toNode);
         return true;
+    }
+
+    //Returns a list of all the edges in the graph.
+    public LinkedList<Edge<T>> getEdges() {
+        var edges = new LinkedList<Edge<T>>();
+        for (var node : nodes) {
+            edges.addAll(node.getEdges());
+        }
+
+        //Remove duplicates
+        var uniqueEdges = new LinkedList<Edge<T>>();
+        for (var edge : edges) {
+            if (!uniqueEdges.contains(edge))
+                uniqueEdges.add(edge);
+        }
+
+        return uniqueEdges;
+    }
+
+    //Returns the count of edges in the graph.
+    public int getEdgeCount() {
+        return getEdges().size();
     }
 
     //Returns a list of the neighbors around a node.
@@ -70,9 +123,11 @@ public class Graph<T> {
             return null;
 
         Node<T> node = getNode(data);
+        assert node != null;
+
         var neighbors = new LinkedList<T>();
-        for (Node<T> n : node.getEdges()) {
-            neighbors.add(n.getData());
+        for (var edge : node.getEdges()) {
+            neighbors.add(edge.getTo().getValue());
         }
         return neighbors;
     }
@@ -80,10 +135,19 @@ public class Graph<T> {
     //Returns true if the Graph contains the Node, false if it does not.
     public boolean contains(T data) {
         for (Node<T> n : nodes) {
-            if (n.getData().equals(data))
+            if (n.getValue().equals(data))
                 return true;
         }
         return false;
+    }
+
+    //Return all the node values in the graph.
+    public LinkedList<T> getNodes() {
+        var nodes = new LinkedList<T>();
+        for (var node : this.nodes) {
+            nodes.add(node.getValue());
+        }
+        return nodes;
     }
 
     //Returns true if there is a path between two nodes, false if not.
@@ -104,12 +168,23 @@ public class Graph<T> {
     }
 
     //Returns the node with the given data.
-    private Node<T> getNode(T data) {
+    public Node<T> getNode(T data) {
         for (Node<T> node : nodes) {
-            if (node.getData().equals(data)) {
+            if (node.getValue().equals(data))
                 return node;
-            }
         }
         return null;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append("Graph: Node Count: ").append(size()).append(" Edge Count: ").append(getEdgeCount()).append(" Nodes: ");
+        for (var node : nodes) {
+            sb.append(node.getValue());
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
---
+++ test/com/ca/two/graph/AlgorithmsTest.java
@@ -0,0 +1,68 @@
+package com.ca.two.graph;
+
+import com.ca.two.graph.Algorithms;
+import com.ca.two.graph.Graph;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmsTest {
+
+    //Graph
+    Graph<String> graph;
+
+    @BeforeEach
+    void setUp() {
+        //Create a graph to test with
+        graph = new Graph();
+        graph.addNode("S");
+        graph.addNode("A", "S");
+        graph.addNode("B", "S", "A");
+        graph.addNode("C", "S");
+        graph.addNode("D", "A", "B");
+        graph.addNode("H", "B");
+        graph.addNode("F", "H", "D");
+        graph.addNode("G", "H");
+        graph.addNode("L", "C");
+        graph.addNode("I", "L");
+        graph.addNode("J", "L", "I");
+        graph.addNode("K", "I", "J");
+        graph.addNode("E", "K", "G");
+
+        //There should be 18 edges in the graph
+        graph.getNode("S").setEdgeWeight(graph.getNode("A"), 7f/8f);
+        graph.getNode("S").setEdgeWeight(graph.getNode("B"), 2f/8f);
+        graph.getNode("A").setEdgeWeight(graph.getNode("B"), 3f/8f);
+        graph.getNode("S").setEdgeWeight(graph.getNode("C"), 3f/8f);
+        graph.getNode("A").setEdgeWeight(graph.getNode("D"), 4f/8f);
+        graph.getNode("B").setEdgeWeight(graph.getNode("H"), 1f/8f);
+        graph.getNode("B").setEdgeWeight(graph.getNode("D"), 4f/8f);
+        graph.getNode("D").setEdgeWeight(graph.getNode("F"), 5f/8f);
+        graph.getNode("H").setEdgeWeight(graph.getNode("G"), 2f/8f);
+        graph.getNode("H").setEdgeWeight(graph.getNode("F"), 3f/8f);
+        graph.getNode("C").setEdgeWeight(graph.getNode("L"), 2f/8f);
+        graph.getNode("L").setEdgeWeight(graph.getNode("I"), 4f/8f);
+        graph.getNode("L").setEdgeWeight(graph.getNode("J"), 4f/8f);
+        graph.getNode("I").setEdgeWeight(graph.getNode("J"), 6f/8f);
+        graph.getNode("I").setEdgeWeight(graph.getNode("K"), 4f/8f);
+        graph.getNode("J").setEdgeWeight(graph.getNode("K"), 4f/8f);
+        graph.getNode("K").setEdgeWeight(graph.getNode("E"), 5f/8f);
+        graph.getNode("G").setEdgeWeight(graph.getNode("E"), 2f/8f);
+    }
+
+    @AfterEach
+    void tearDown() {
+        //Clear the graph
+        graph = null;
+    }
+
+    @Test
+    void dijkstra() {
+        //Test the Dijkstra algorithm
+        var result = Algorithms.Dijkstra(graph, "S", "E");
+        var expected = new String[]{"S", "B", "H", "G", "E"};
+        assertArrayEquals(expected, result.toArray());
+    }
+}
