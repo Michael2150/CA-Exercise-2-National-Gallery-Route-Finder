752f49d56ee33ddbcce02b5aa81e7e482f5e1b90
Michael2150 <52236517+Michael2150@users.noreply.github.com>
2022-04-11 21:27:48 +0100

    sync

---
+++ src/main/java/com/ca/two/models/Room.java
@@ -0,0 +1,58 @@
+package com.ca.two.models;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Room {
+    private UUID id;
+    private String name;
+    private String description;
+    private String image_url;
+    private
+
+    public Room() {
+        this.id = UUID.randomUUID();
+    }
+
+    // Getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Room)) return false;
+        Room room = (Room) o;
+        return Objects.equals(getId(), room.getId());
+    }
+}
